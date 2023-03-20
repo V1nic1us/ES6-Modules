1ab960c5ea444159d4500f2a5cb07233e4295dcd
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-03-20 17:05:28 -0400

    ES6 Modules - Import/Export

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// import {name as nome, sobrenome, age, soma} from './modulo1';
+// import * as MyModule from './modulo1.js'
+// import useDefault from '.modulo1';
+import soma, {name as nome, lastName, age} from './modulo1';
+
+// const name = 'Vinicius';
+console.log(soma(5, 40));
+// console.log(MyModule);
+console.log(nome, lastName, age);
+// console.log(soma(5,5));
---
+++ src/modulo1.js
@@ -0,0 +1,12 @@
+export const name = 'Marcus';
+export const lastName = 'Marques';
+export const age = 22;
+const cpf = 'asdasd';
+
+export function soma(x, y) {
+    return x+y;
+}
+
+export default (x, y) => x * y;
+
+// export {name, lastName as sobrenome, age, soma };
